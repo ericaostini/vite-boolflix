3b28e9dfb884a3c768b92e3448820cee888c48aa
ericaostini <ericaostini14@gmail.com>
2023-11-15 15:21:52 +0100

    create component header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <template>
   <div>
-    ciao
+    <HeaderComponent />
+    <h2>Movies</h2>
+    <div class="row">
+      <div class="col-12 col-md-4 col-lg-3" v-for="(movie, index) in store.listFilm" :key="movie.id">
+        {{ movie.title }}
+        <img :src="'https://image.tmdb.org/t/p/w185' + movie.poster_path" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
 import { store } from './data/store';
 import axios from 'axios';
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       store
+    };
+  },
+  methods: {
+    getSeries() {
+      const seriesurl = this.store.apiUrl + this.store.endPoint.series;
+      console.log(seriesurl);
+      axios.get(seriesurl, { params: this.store.params }).then((res) => {
+        console.log(res.data.results);
+        this.store.listSeries = res.data.results;
+        console.log(this.store.listSeries);
+      });
+      const movieurl = this.store.apiUrl + this.store.endPoint.movie;
+      console.log(movieurl);
+      axios.get(movieurl, { params: this.store.params }).then((res) => {
+        console.log(res.data.results);
+        this.store.listFilm = res.data.results;
+        console.log(this.store.listFilm);
+      });
     }
   },
   created() {
-    const url = store.apiUrl + this.store.endPoint.series;
-    console.log(url);
-    axios.get(url, { params: this.store.params }).then((res) => {
-      console.log(res.data.results)
-    })
+    this.getSeries();
   },
+  components: { HeaderComponent }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use './assets/style/partials/variables' as *;
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <div class="d-flex justify-content-between align-items-center">
+            <h1 class="text-danger">Boolflix</h1>
+            <form class="row align-items-center">
+                <div class="col-auto">
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-danger">Search</button>
+                </div>
+            </form>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '.././assets/style/partials/variables' as *;
+
+header {
+    height: 100px;
+    background-color: $col-dark;
+    padding: 20px;
+}
+</style>
