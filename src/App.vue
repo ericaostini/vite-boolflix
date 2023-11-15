de0d2c39dae85f86899fbf7b9aee059e4e6f2520
ericaostini <ericaostini14@gmail.com>
2023-11-15 12:22:00 +0100

    start commit, milestone 0 complete

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get(store.apiUrl).then((response) => {
+        console.log(response.data);
+      })
+    }
+  },
+  created() {
+    this.getApi()
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/data/store.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3'
+});
