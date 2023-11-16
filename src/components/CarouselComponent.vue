a4a99534d70a94aa33f2d0ac6b723d190846253c
ericaostini <ericaostini14@gmail.com>
2023-11-16 11:49:14 +0100

    create carouselComponent

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="wrapper">
+        <section id="section1">
+            <a href="#section4"><i class="fa-solid fa-chevron-left"></i></a>
+            <div class="item" v-for="n in 5">
+                <img :src="image">
+            </div>
+            <a href="#section2"><i class="fa-solid fa-chevron-right"></i></a>
+        </section>
+        <section id="section2">
+            <a href="#section1"><i class="fa-solid fa-chevron-left"></i></a>
+            <div class="item" v-for="n in 5">
+                <img :src="image">
+            </div>
+            <a href="#section3"><i class="fa-solid fa-chevron-right"></i></a>
+        </section>
+        <section id="section3">
+            <a href="#section2"><i class="fa-solid fa-chevron-left"></i></a>
+            <div class="item" v-for="n in 5">
+                <img :src="image">
+            </div>
+            <a href="#section4"><i class="fa-solid fa-chevron-right"></i></a>
+        </section>
+        <section id="section4">
+            <a href="#section3"><i class="fa-solid fa-chevron-left"></i></a>
+            <div class="item" v-for="n in 5">
+                <img :src="image">
+            </div>
+            <a href="#section1"><i class="fa-solid fa-chevron-right"></i></a>
+        </section>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CarouselComponent'
+}
+</script>
+
+<style lang="scss" scoped></style>
